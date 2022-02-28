1f56b504d9d12e511a44b67b1da621a435ecd24b
adritadeb <adritad2019@gmail.com>
2022-03-01 01:16:24 +0600

    phone result done

--- js/script.js
+++ js/script.js
@@ -7,5 +7,27 @@ const searchPhone = () => {
         .then(data => displayPhone(data.data));
 };
 const displayPhone = phones => {
-    console.log(phones);
+    const phoneContainer = document.getElementById('phone-container');
+    phoneContainer.textContent = '';
+    phones.forEach(phone => {
+        console.log(phone);
+        const div = document.createElement('div');
+        div.innerHTML = ` <div class="col">
+        <div class="card h-100">
+            <img src="${phone.image}" class="card-img-top w-50 mt-2 ms-2" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Name: ${phone.phone_name}</h5>
+                <h5 class="card-title">Brand: ${phone.brand}</h5>
+            </div>
+            <button onclick="loadDetails('${phone.slug}')" id="details-btn" class="btn w-50 ms-2 mb-3">Explore</button>
+        </div>
+    </div>`;
+        phoneContainer.appendChild(div);
+    });
+};
+const loadDetails = phoneInfo => {
+    const url = `https://openapi.programming-hero.com/api/phone/${phoneInfo}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => console.log(data));
 };
